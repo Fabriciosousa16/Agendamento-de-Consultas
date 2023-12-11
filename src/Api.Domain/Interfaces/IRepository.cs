d4679b758be4c25811abd41430e2cf87f81b1559
Fabriciosousa16 <fabriciosousa16@hotmail.com>
2023-12-11 08:30:21 -0300

    Implementando Interface IRepository e Class BaseRepository para servir como base para realizar o CRUD via API no banco de dados

---
+++ src/Api.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+
+namespace Api.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
